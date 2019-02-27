c2b8532501af692d7c0646348a0ad2d175fb4277
Denis Panov <v-depano@microsoft.com>
2019-02-27 12:08:46 +0300

    test

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -1,12 +1,2 @@
 #!/usr/bin/env bash
 
-echo "---------------Java-7-8-9-10-11--------------"
-echo $JAVA_HOME_7_X64
-echo $JAVA_HOME_8_X64
-echo $JAVA_HOME_9_X64
-echo $JAVA_HOME_10_X64
-echo $JAVA_HOME_11_X64
-echo "---------------Java-Home---------------------"
-echo $JAVA_HOME
-echo "---------------------------------------------"
-java -version
